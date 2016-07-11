591b738fecaa280cf6f23ab0d0a1b59188d72747
Denis Vasquez <denis.vasquez@jalasoft.com>
2016-07-11 18:20:41 -0400

    order posts

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,7 +6,11 @@ Template.postSubmit.events({
             publication: $(e.target).find('[name=publicationText]').val()
         };
 
-        post._id = Posts.insert(post);
-        Router.go('postPage', post);
+        Meteor.call('postInsert', post, function(error, result) {
+            // display the error to the user and abort
+            if (error)
+                return alert(error.reason);
+            Router.go('postPage', {_id: result._id});
+        });
     }
 });
