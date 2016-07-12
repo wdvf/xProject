48fda02de5cf20e0cf6108dedd4a379d5df6d967
Denis Vasquez <denis.vasquez@jalasoft.com>
2016-07-12 16:43:44 -0400

    make comments functionality added

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,5 +1,8 @@
 Template.postItem.helpers({
     ownPost: function() {
         return this.userId === Meteor.userId();
+    },
+    commentsCount: function() {
+        return Comments.find({postId: this._id}).count();
     }
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,53 @@
 if (Posts.find().count() === 0) {
-    Posts.insert({
-        author: 'Denis Vasquez',
-        publication: 'Hola a todos, Hola a todos, Hola a todos, Hola a todos, Hola a todos, Hola a todos, Hola a todos'
+
+    var now = new Date().getTime();
+
+    // create two users
+    var biancaId = Meteor.users.insert({
+        profile: { name: 'Bianca Vasquez' }
+    });
+    var bianca = Meteor.users.findOne(biancaId);
+
+
+    var elvisId = Meteor.users.insert({
+        profile: { name: 'Elvis Munhoz' }
+    });
+    var elvis = Meteor.users.findOne(elvisId);
+
+    var biancaPostId = Posts.insert({
+        userId: bianca._id,
+        author: bianca.profile.name,
+        publication: 'Hola a todos, Hola a todos, Hola a todos, Hola a todos, Hola a todos, Hola a todos, Hola a todos',
+        submitted: new Date(now - 7 * 3600 * 1000)
+    });
+
+    Comments.insert({
+        postId: biancaPostId,
+        userId: elvis._id,
+        author: elvis.profile.name,
+        submitted: new Date(now - 5 * 3600 * 1000),
+        body: 'Interesting project Bi, can I get involved?'
+    });
+
+    Comments.insert({
+        postId: biancaPostId,
+        userId: bianca._id,
+        author: bianca.profile.name,
+        submitted: new Date(now - 3 * 3600 * 1000),
+        body: 'You sure can Elvis!'
     });
 
     Posts.insert({
-        author: 'Bianca Vasquez',
-        publication: 'De compras!'
+        userId: bianca._id,
+        author: bianca.profile.name,
+        publication: 'De compras!',
+        submitted: new Date(now - 4 * 3600 * 1000)
     });
 
     Posts.insert({
-        author: 'Denis Vasquez',
-        publication: 'Aburrido...'
+        userId: elvis._id,
+        author: elvis.profile.name,
+        publication: 'Aburrido...',
+        submitted: new Date(now - 10 * 3600 * 1000)
     });
 }
